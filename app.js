494b757d4ef397940c96954b375034a5f9ff4cd8
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-18 21:39:48 +0100

    message function

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ const https = require('https');
 // Require http module for status code 
 
 const http = require('http');
+
+// Funcitons
+
+// message 
+
+function printMessage(place, degree) {
+    const msg = `Current temperature in ${place} is ${degree}`;
+    console.log(msg);
+}
+
