e74d7e7d1b12926d80bcd572283fa8c3df228110
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-20 21:29:51 +0100

    get argument

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 const weather = require('./weather');
 
 // Join multiple values passed as arguments and replace all spaces with underscore
-const query = process.argv.slice(2).join('_').replace(' ', '_');
+const query = process.argv.slice(2);
 
 weather.get(query);
