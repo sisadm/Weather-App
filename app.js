2d2fb49e36a44bec599d8818dfb15baff6b84daa
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2021-10-20 21:10:00 +0100

    add get() function to app from weather.js

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ const weather = require('./weather');
 // Join multiple values passed as arguments and replace all spaces with underscore
 const query = process.argv.slice(2).join('_').replace(' ', '_');
 
+weather.get(query);
